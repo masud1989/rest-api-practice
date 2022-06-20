4caa487093f61df6ed29ec80dec96a1474adbde5
masud1989 <mranasrj@gmail.com>
2022-06-20 12:55:47 +0600

    undefined-route

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const router = require('./src/routes/api');
 
 const app = new express();
 
+// Routes 
 app.use('/api/v1', router);
+// http://localhost:5000/api/v1/hello-post
+
+// Undefined Route 
+app.use('*', (req, res) => {
+    res.status(404).json({status:"failed", data: "Not found"})
+})
 
 module.exports = app;
