831bbf44c802d9f32a1a0b20b9857e5c89143784
masud1989 <mranasrj@gmail.com>
2022-06-20 19:58:34 +0600

    data-insert-with-default value-virsionkey

--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const DataSchema = mongoose.Schema({
     name:String,
     model:String,
-    quantity:String,
-    price:String,
+    quantity:{type:Number, default:5},
+    price:{type:Number},
     ratings:String,
     image:String
-})
+},{versionKey:false})
 
 const ProductsModel = mongoose.model('products',DataSchema);
 module.exports = ProductsModel;
